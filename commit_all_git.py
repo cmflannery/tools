7f8fc7f2f3e756ee027ea0a5aa0f1acf4e2c060a
Cameron Flannery <cflannery@Camerons-MacBook-Pro.local>
2017-03-17 14:29:49 -0700

    automated commit with python tools

--- commit_all_git.py
+++ commit_all_git.py
@@ -8,28 +8,37 @@ def parsey():
     parser = argparse.ArgumentParser(description='Commit and push git repositories in working directory.')
     parser.add_argument('-repo', dest='repo', metavar='N', type=str, nargs='*',
                         help='Array of repositories to commit and push')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                        const=sum, default=max,
-                        help='sum the integers (default: find the max)')
+    parser.add_argument('-A', dest='all', action='store_true',
+                        help='Push and Commit all repositories. Void if -repo flag is called')
     args = parser.parse_args()
     return args
 
-
-def main():
-    args = parsey()
+def commit(repos, dirsincwd):
     currentdir = os.getcwd()
-    dirsincwd = os.listdir('.')
-    for dirname in args.repo:
+    for dirname in repos:
         if os.path.isdir(dirname) and dirname in dirsincwd:
             dirpath = os.path.join(currentdir, dirname)
             os.chdir(dirpath)  # enter git repository directory
-            print('Updating: ', dirname)
+            print('\nCommitting and Pushing: ', dirname)
             subprocess.call('git add -A', shell=True)  # update
             subprocess.call('git commit -m \"automated commit with python tools\"', shell=True)  # update
             subprocess.call('git push', shell=True)
             os.chdir(currentdir)  # return to main working directory
         else:
             print(dirname, 'is not a repository in the working directory')
+
+
+def main():
+    args = parsey()
+    dirsincwd = os.listdir('.')
+    if args.all:
+        commit(dirsincwd, dirsincwd)
+        return 0
+    if args.repo != None:
+        commit(args.repo, dirsincwd)
+    elif args.repo == None:
+        print('Error: You must enter at least one repository, or add the -A flag')
+        return 1
     print('All done!')
 
 
