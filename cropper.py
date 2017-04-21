8faba8abd50d1c22ac589a7dbecdcdc98f3ece35
cflannery <cmflannery@ucsd.edu>
2017-04-21 00:27:06 -0700

    automated commit with python tools

--- cropper.py
+++ cropper.py
@@ -1,4 +1,4 @@
-`from PIL import Image
+from PIL import Image
 import argparse
 import subprocess
 # from future import print_function
--- pytut.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-x = np.linspace(-50, 50)
-y = x**2
-
-with plt.xkcd():
-    plt.plot(x, y)
-    plt.show()
